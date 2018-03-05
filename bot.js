55bca56494e3ff09401c7f267b9aee6ff2af472f
nanjohtk <36926075+nanjohtk@users.noreply.github.com>
2018-03-05 21:46:52 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'ヨーソロー!!') {
+    	message.reply('からの？敬礼!!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
